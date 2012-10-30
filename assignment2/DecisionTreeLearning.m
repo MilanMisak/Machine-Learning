cf69ff3bfeeac473539b240d3dc302a46bff0dfd
Jack Bracewell <jb2910@fusion04.doc.ic.ac.uk>
2012-10-30 21:20:09 +0000

    Trees visualised and working

--- assignment2/DecisionTreeLearning.m
+++ assignment2/DecisionTreeLearning.m
@@ -3,22 +3,24 @@ function [ tree ] = DecisionTreeLearning( examples, attributes, binaryTargets)
 
 if range(binaryTargets) == 0
     % All binary targets are the same
-    tree.kids = [];
+    tree.kids = cell(0);
     tree.class = binaryTargets(1);
 elseif isempty(attributes)
     % There are no attributes to split the tree on
-    tree.kids = [];
+    tree.kids = cell(0);
     tree.class = MajorityValue(binaryTargets);
 else
     bestAttribute = ChooseBestDecisionAttribute(examples, attributes, binaryTargets);
     bestAttributeIndex = 0;
-    for i=1:size(attributes)
+
+    size(attributes, 2)
+    for i=1:size(attributes, 2)
        if attributes(i) == bestAttribute
            bestAttributeIndex = i;
        end
     end
     
-    kids = [];
+    kids = cell(0);
     
     for u=0:1
         newExamples = [];
@@ -37,13 +39,13 @@ else
         end
         
         if isempty(newExamples)
-            tree.kids = [];
+            tree.kids = cell(0);
             tree.class = MajorityValue(binaryTargets);
             return
         else
             attributes(find(attributes ~= bestAttribute));
-            kid = DecisionTreeLearning(newExamples, attributes(find(attributes ~= bestAttribute)), newBinaryTargets);
-            kids = [kids; kid];
+            kid = DecisionTreeLearning(newExamples, attributes(find(attributes ~= bestAttribute)), newBinaryTargets);            
+            kids{size(kids, 1) + 1} = kid;
         end
     end
     
--- assignment2/Main.m
+++ assignment2/Main.m
@@ -3,13 +3,18 @@ cleandata = load('cleandata_students.mat');
 
 
 %Create the 6 target vectors
-targets = cell(6)
+targets = cell(6);
 for i=1:6
     targets{i} = zeros(size(cleandata.y));
     for j=1:size(cleandata.y)
         targets{i}(j) = cleandata.y(j) == i;
     end
 end
+
+attributes = 1:1:45;
+S = DecisionTreeLearning(cleandata.x, attributes, targets{1}); 
+DrawDecisionTree(S)
+
 
 
             
