c204bd554b7ce6f0f2b2f40b5e75004459907b1b
Craig Ellis <cellis_222@hotmail.com>
2012-11-01 15:13:22 +0000

    treeClassify done, methinks

---
+++ assignment2/TreeClassify.m
@@ -0,0 +1,13 @@
+function [ classification ] = TreeClassify( tree, example )
+%TreeClassify will traverse a tree, and classify the example as 1 or 0
+
+if isempty(tree.op)
+    classification=tree.class;
+else
+    if example(tree.op)==0
+        classification = TreeClassify( tree.kids(0), example );
+    else
+        classification = TreeClassify( tree.kids(1), example );
+    end
+end
+
