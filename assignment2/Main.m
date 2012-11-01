a53b58f1d01e773cadb6846d7fcf89f4ee31f832
Craig Ellis <cellis_222@hotmail.com>
2012-11-01 20:16:57 +0000

    decision trees drawn each time

--- assignment2/Main.m
+++ assignment2/Main.m
@@ -16,6 +16,11 @@ attributes = 1:1:45;
 
 CrossValidate(cleandata.x, cleandata.y)
 
-
+trees = cell(1, 6);
+for i=1:6
+    targetVector = targets{i}(1:size(targets{i}, 1));
+    trees{i} = DecisionTreeLearning(cleandata.x, 1:1:45, targetVector);
+    DrawDecisionTree(trees{i});
+end
 
             
