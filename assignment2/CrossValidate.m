12288b9878e2feaf471990c9ccff3f41f5628ce8
Jack Bracewell <jb2910@fusion23.doc.ic.ac.uk>
2012-11-01 14:25:40 +0000

    Fixed missing newTarget

--- assignment2/CrossValidate.m
+++ assignment2/CrossValidate.m
@@ -4,7 +4,7 @@ function [ errorEstimate ] = CrossValidate( examples, labels )
 
 errorEstimate = 0;
 first = 1;
-last = 1;
+last = 0;
 
 targets = cell(6);
 for i=1:6
@@ -16,21 +16,21 @@ end
 
 for i=1:10
     % first will mark the index of the start of the fold, and last the end
-    first = last;
+    first = last + 1;
     last = round(size(examples, 1) / i);
     foldSize = last - first;
     
     % split the examples from the fold
-    newExamples = examples(~ismember(1:size(examples, 1), [first:last]), :);
+    newExamples = examples(~ismember(1:size(examples, 1), (first:last)), :);
     newTestSet = examples(ismember(1:size(examples, 1), [first:last]), :);
     
     % create the 6 trees for the fold, and classify the test set for each
     trees = cell(6);
     classifications = cell(6);
     correctClassifications = cell(6);
-    for n=1:6
-        examplesTargets = targets{n}(~ismember(1:size(newTargets, 1), [first:last]), :);
-        testSetTargets = targets{n}(ismember(1:size(newTargets, 1), [first:last]), :);
+    for n=1:
+        examplesTargets = targets{n}(~ismember(1:size(targets{n}, 2), (first:last)), :);
+        testSetTargets = targets{n}(ismember(1:size(targets{n}, 2), (first:last)), :);
         
         % train a tree on the examples
         trees{n} = DecisionTreeLearning(newExamples, attributes, examplesTargets);
