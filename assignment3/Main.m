98f1aa949fe22a9c916ea116b6a0f4e4355ed8f4
Jack Bracewell <jb2910@pixel24.doc.ic.ac.uk>
2012-11-15 14:46:56 +0000

    Mush them together

--- assignment3/Main.m
+++ assignment3/Main.m
@@ -1,18 +1,35 @@
 cleandata = load('cleandata_students.mat');
 
-[x2, y2] = ANNdata(cleandata.x, cleandata.y)
+[x2, y2] = ANNdata(cleandata.x, cleandata.y);
 
 
 % Part 3 stuff
 x = x2;
 y = y2;
 
-test_inputs = x(:, 1:100);
-test_targets = y(:, 1:100);
+testInputs = x(:, 1:100);
+testTargets = y(:, 1:100);
 
 [net] = feedforwardnet(6);
 [net] = configure(net, x, y);
 net.trainParam.epochs = 100;
-[net] = train(net, test_inputs, test_targets);
-Y = sim(net, test_inputs);
-%plot(test_inputs, test_targets, test_inputs, Y, 'r.');
+[net] = train(net, testInputs, testTargets);
+Y = sim(net, testInputs);
+
+nets = cell(1, 6);
+simulations = cell(1, 6);
+
+for i=1:6
+    trainingTargets = x(i, :);
+    [nets{i}] = feedforwardnet(6);
+    [nets{i}] = configure(nets{i}, x2, y2);
+    nets{i}.trainParam.epochs = 100;    
+    nets{i} = train(nets{i}, testInputs, testTargets);
+    simulations{i} = sim(nets{i}, testInputs);
+end
+
+
+
+
+
+
