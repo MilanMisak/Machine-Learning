a88392ace759a4c15bf8d461c78d8dde5f22cd84
Milan Misak <milan.misak10@imperial.ac.uk>
2012-11-15 12:39:02 +0000

    It does things

--- assignment3/Main.m
+++ assignment3/Main.m
@@ -1,3 +1,15 @@
 cleandata = load('cleandata_students.mat');
 
 [x2, y2] = ANNdata(cleandata.x, cleandata.y)
+
+
+% Part 3 stuff
+x = x2;
+y = y2;
+
+test_inputs = x(:, 1:100);
+test_targets = y(:, 1:100);
+
+[net] = feedforwardnet(50);
+[net] = configure(net, x, y);
+[net] = train(net, test_inputs, test_targets);
