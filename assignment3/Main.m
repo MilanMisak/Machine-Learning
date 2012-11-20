12802bd418240f29d1c4807188be872ea4b88c8b
Jack Bracewell <jbtwentythree@gmail.com>
2012-11-20 00:41:14 +0000

    Rawhide

--- assignment3/Main.m
+++ assignment3/Main.m
@@ -1,3 +1,7 @@
+
+
+
+
 cleandata = load('cleandata_students.mat');
 
 [x2, y2] = ANNdata(cleandata.x, cleandata.y);
@@ -8,18 +12,18 @@ n = size(x, 2);
 %f = floor(0.67*n)
 %c = ceil(0.67*n)
 
-testInputs  = x(:, 1:floor(0.67*n));
-testTargets = y(:, 1:floor(0.67*n));
-validationInputs  = x(:, (floor(0.67*n) + 1):n);
-validationTargets = y(:, (floor(0.67*n) + 1):n);
+%testInputs  = x(:, 1:floor(0.67*n));
+%testTargets = y(:, 1:floor(0.67*n));
+%validationInputs  = x(:, (floor(0.67*n) + 1):n);
+%validationTargets = y(:, (floor(0.67*n) + 1):n);
 
 % 6-output network
-[net] = feedforwardnet(6);
-[net] = configure(net, x, y);
-net.trainParam.epochs = 100;
-[net, tr] = train(net, testInputs, testTargets);
+%[net] = feedforwardnet(6);
+%[net] = configure(net, x, y);
+%net.trainParam.epochs = 100;
+%[net, tr] = train(net, testInputs, testTargets);
 
-predictions = testANN(net, validationInputs);
+%predictions = testANN(net, validationInputs);
 
 
 
@@ -27,23 +31,33 @@ predictions = testANN(net, validationInputs);
 nets = cell(1, 6);
 trs = cell(1, 6);
 simulations = cell(1, 6);
+performance = ones(1, 6);
+best_performance = 0
 
-for i=1:6
+
+for i=1:1
     trainingTargets = testTargets(i, :);
-    [nets{i}] = feedforwardnet([15,20], 'traingdx');
-    [nets{i}] = configure(nets{i}, testInputs, trainingTargets);
-    %nets{i}.layers{1}.transferFcn = '';
-    %nets{i}.layers{2}.transferFcn = '';
-    nets{i}.trainParam.mc = 0.95;
-    nets{i}.trainParam.epochs = 1000;
-    nets{i}.trainParam.lr = 0.015;
-    [nets{i}, trs{i}] = train(nets{i}, testInputs, trainingTargets);
-    simulations{i} = sim(nets{i}, testInputs);
-
-    fprintf('Best performance: %f\n', trs{i}.best_perf);
+    for j=2:15
+        vector = j; 
+        [nets{i}, performance(i), trs{i}, simulations{i}] = get_fitness(x, y, nets{i}, performance(i), trs{i}, simulations{i}, vector);
+        for k=2:15
+            vector = [j,k]; 
+            [nets{i}, performance(i), trs{i}, simulations{i}] = get_fitness(x, y, nets{i}, performance(i), trs{i}, simulations{i}, vector);
+            for l=2:15
+                vector = [j,k,l]; 
+                [nets{i}, performance(i), trs{i}, simulations{i}] = get_fitness(x, y, nets{i}, performance(i), trs{i}, simulations{i}, vector);                         
+            end            
+        end
+    end
+    best_performance = best_performance + performance(i);
 end
 
 
+
+best_performance
+trs{i}
 %plotperform(tr);
 
 %plottrainstate(tr);
+
+
