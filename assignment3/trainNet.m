0bf6786fd11d5175d87981fadb514aea53d01226
Milan Misak <misakmilan@gmail.com>
2012-11-21 19:35:17 +0000

    Stuff

--- assignment3/trainNet.m
+++ assignment3/trainNet.m
@@ -2,26 +2,53 @@ function [ net_out ] = trainNet( trainingInputs, trainingTargets, hiddenLayerNod
 %trainNet Trains a neural network.
 
 best = 100;
-for i=1:10
-    [net] = feedforwardnet(hiddenLayerNodes, 'trainrp');
-    [net] = configure(net, trainingInputs, trainingTargets);
+max_fail = 15;
+escape = 0;
+perf = 100;
+old_perf = 101;
+%old_net = none
 
-    for j=1:size(hiddenLayerNodes, 1)
-        net.layers{j}.transferFcn = 'tansig';    
+while ~escape
+    for i=1:1
+        net = createNet(trainingInputs, trainingTargets, hiddenLayerNodes, max_fail);
+        [net, tr] = train(net, trainingInputs, trainingTargets);
+
+        vperf = tr.vperf(size(tr.vperf, 2));
+        if perf > vperf
+            perf = vperf;
+            best_net = net;
+        end
     end
-
-    net.trainParam.epochs = 100;
-    net.trainParam.showWindow = 0;
-
-    net.divideParam.trainRatio = 67/100;
-    net.divideParam.valRatio = 33/100;
-    net.divideParam.testRatio = 0;
-   
-    
-    [net, tr] = train(net, trainingInputs, trainingTargets);
-
-    if tr.vperf(size(tr.vperf, 2)) < best
-        net_out = net;
-
+    if old_perf < perf
+        escape = 1;
+    else
+        old_perf = perf;
+        max_fail = max_fail - 1;
+        old_net = best_net;
+    end
 end
+
+net_out = old_net;
+end
+
+
+function [ net ] = createNet ( trainingInputs, trainingTargets, hiddenLayerNodes, max_fail)
+%createNet Helper function for creating a neural network.
+
+[trainInd, valInd, testInd] = dividerand(size(trainingTargets, 2), 67/100, 33/100, 0);
+[net] = feedforwardnet(hiddenLayerNodes, 'trainrp');
+[net] = configure(net, trainingInputs, trainingTargets);
+
+for j=1:size(hiddenLayerNodes, 1)
+    net.layers{j}.transferFcn = 'tansig';
+end
+
+net.trainParam.epochs = 100;
+net.trainParam.showWindow = 0;
+%net.trainParam.max_fail = max_fail;
+
+net.divideFcn = 'divideind';
+net.divideParam.trainInd = trainInd;
+net.divideParam.valInd = valInd;
+net.divideParam.testInd = testInd;
 end
