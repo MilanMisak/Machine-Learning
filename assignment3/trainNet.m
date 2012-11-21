42bb91051db876589af9ebb1e107f98bf0e4318d
Jack Bracewell <jbtwentythree@gmail.com>
2012-11-21 18:15:18 +0000

    give up

--- assignment3/trainNet.m
+++ assignment3/trainNet.m
@@ -1,19 +1,27 @@
-function [ net ] = trainNet( trainingInputs, trainingTargets, hiddenLayerNodes )
+function [ net_out ] = trainNet( trainingInputs, trainingTargets, hiddenLayerNodes )
 %trainNet Trains a neural network.
 
-[net] = feedforwardnet(hiddenLayerNodes, 'trainrp');
-[net] = configure(net, trainingInputs, trainingTargets);
+best = 100;
+for i=1:10
+    [net] = feedforwardnet(hiddenLayerNodes, 'trainrp');
+    [net] = configure(net, trainingInputs, trainingTargets);
 
-for i=1:size(hiddenLayerNodes, 1)
-    net.layers{i}.transferFcn = 'tansig';    
+    for j=1:size(hiddenLayerNodes, 1)
+        net.layers{j}.transferFcn = 'tansig';    
+    end
+
+    net.trainParam.epochs = 100;
+    net.trainParam.showWindow = 0;
+
+    net.divideParam.trainRatio = 67/100;
+    net.divideParam.valRatio = 33/100;
+    net.divideParam.testRatio = 0;
+   
+    
+    [net, tr] = train(net, trainingInputs, trainingTargets);
+
+    if tr.vperf(size(tr.vperf, 2)) < best
+        net_out = net;
+
 end
-
-net.trainParam.epochs = 100;
-net.trainParam.showWindow = 0;
-
-net.divideParam.trainRatio = 67/100;
-net.divideParam.valRatio = 33/100;
-net.divideParam.testRatio = 0;
-        
-[net, tr] = train(net, trainingInputs, trainingTargets);
 end
