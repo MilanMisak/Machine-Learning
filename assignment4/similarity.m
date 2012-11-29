8af52e850db3a5d71939677cf0f5d87c4728183f
cellis222 <cellis_222@hotmail.com>
2012-11-29 20:45:50 +0000

    woo, similarity

--- assignment4/similarity.m
+++ assignment4/similarity.m
@@ -1,5 +1,12 @@
 function [ similarity ] = similarity( case1, case2 )
 % similarity computes the similarity of 2 cases.
     % TODO - at least 3 different measures
-    similarity = 0;
+    matching_attributes = 0;
+    for i=1:size(case1.problem)
+        if ismember(case1.problem(i), case2.problem)
+            matching_attributes = matching_attributes + 1;
+        end
+    end
+    unmatched_case2_attributes = size(case2.problem)-matching_attributes;
+    similarity = matching_attributes/(size(case1.problem)+unmatched_case2_attributes);
 end
