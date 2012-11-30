dc2e9b705a816875082e22c58d63eaf69203b24f
Milan Misak <misakmilan@gmail.com>
2012-11-30 17:58:24 +0000

    Better retrieve

--- assignment4/retrieve.m
+++ assignment4/retrieve.m
@@ -2,16 +2,32 @@ function [ outputcase ] = retrieve( cbr, newcase )
 % retrieve retrieves a case that matches best with newcase from the given
 % CBR system.
     bestsimilarity = Inf;
-    bestsimilarityindex = 0;
+    bestsimilarityarray = {};
     for i=1:size(cbr.cases, 2)
         othercase = cbr.cases{i};
         newsimilarity = similarity(othercase, newcase);
 
         if newsimilarity < bestsimilarity
            bestsimilarity = newsimilarity;
-           bestsimilarityindex = i;
+           bestsimilarityarray = {othercase};
+        elseif newsimilarity == bestsimilarity
+            bestsimilarityarray{size(bestsimilarityarray, 1) + 1} = othercase;
         end
     end
+    
+    typicalities = zeros(1, 6);
+    for i=1:size(bestsimilarityarray, 2)
+        candidatecase = bestsimilarityarray{1, i};
+        typicalities(1, candidatecase.solution) = typicalities(1, candidatecase.solution) + candidatecase.typicality;
+    end
+    
+    [C, maxtypicalityindex] = max(typicalities);
 
-    outputcase = cbr.cases{bestsimilarityindex};
+    for i=1:size(bestsimilarityarray, 2)
+        candidatecase = bestsimilarityarray{1, i};
+        if candidatecase.solution == maxtypicalityindex
+            outputcase = candidatecase;
+            return;
+        end
+    end
 end
