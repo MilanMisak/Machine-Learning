ca0195cb3afc06dd4aa31f281647ba47371e2ab0
Milan Misak <misakmilan@gmail.com>
2012-11-30 15:33:01 +0000

    Retrieve should work

--- assignment4/retrieve.m
+++ assignment4/retrieve.m
@@ -1,6 +1,17 @@
 function [ outputcase ] = retrieve( cbr, newcase )
 % retrieve retrieves a case that matches best with newcase from the given
 % CBR system.
-    % TODO
-    outputcase = [];
+    bestsimilarity = -Inf;
+    bestsimilarityindex = 0;
+    for i=1:size(cbr.cases, 2)
+        othercase = cbr.cases{i};
+        newsimilarity = similarity(othercase, newcase);
+
+        if newsimilarity < bestsimilarity
+           bestsimilarity = newsimilarity;
+           bestsimilarityindex = i;
+        end
+    end
+
+    outputcase = cbr.cases{bestsimilarityindex};
 end
