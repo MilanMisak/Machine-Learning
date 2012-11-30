0536740f2726c4e59da84126c700591783ee2a87
Milan Misak <misakmilan@gmail.com>
2012-11-30 12:39:31 +0000

    Fixed MakeCase

--- assignment4/MakeCase.m
+++ assignment4/MakeCase.m
@@ -1,10 +1,13 @@
 function [ outputcase ] = MakeCase( inputvector, scalarlabel )
-%MakeCase TODO: makes a case from these input things
+%MakeCase makes a case from these input vector and scalar label.
     auvector = [];
+
     for i=1:size(inputvector, 2)
-        if inputvector[i] == 1
-            auvector = auvector + [i];
+        inputvector(1, i)
+        if inputvector(1, i) == 1
+            auvector = [auvector; i];
         end
     end
+
     outputcase = struct('problem', auvector, 'solution', scalarlabel, 'typicality', 1);
 end
