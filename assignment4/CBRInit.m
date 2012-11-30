a9c546b5343df80e87541b00cd53b8218a21f99e
cellis222 <cellis_222@hotmail.com>
2012-11-30 00:43:14 +0000

    this should (might) work

--- assignment4/CBRInit.m
+++ assignment4/CBRInit.m
@@ -4,7 +4,37 @@ function [ cbr ] = CBRInit( x, y )
     % TODO
     cbr.cases = {};
     for i=1:size(x, 1)
-        % TODO - check if a case already exists and then update typicality
-        cbr.cases{i} = MakeCase(x(i, :), y(i));
+        existingcase = ExistsInCellArray(cbr.cases, x(i, :));
+        if existingcase == -1
+            cbr.cases{i} = MakeCase(x(i, :), y(i));
+        else
+            cbr.cases{existingcase}.typicality = cbr.cases{existingcase}.typicality + 1;
+        end
     end
+end
+
+
+function [ exists ] = ExistsInCellArray( cellarray, attributevector )
+    
+    if size(cellarray, 1)==0
+        exists = -1;
+        return;
+    end
+
+    auvector = [];
+    for i=1:size(attributevector, 2)
+        if attributevector[i] == 1
+            auvector = auvector + [i];
+        end
+    end
+    
+    for j=1:size(cellarray, 1)
+        if cellarray(j).problem==auvector
+            exists = j;
+            return;
+        end
+    end
+    
+    exists = -1;
+    return;
 end
