046ee8a70ee84f441fa96b55095f8461bb1da7b3
Jack Bracewell <jbtwentythree@gmail.com>
2012-11-30 18:11:43 +0000

    intersect

--- assignment4/similarity.m
+++ assignment4/similarity.m
@@ -1,7 +1,7 @@
 function [ similarity ] = simple_similarity( case1, case2 )
 % similarity computes the similarity of 2 cases.
     % TODO - at least 3 different measures
-    similarity = size(union(case1, case2));
+    similarity = size(intersect(case1, case2));
 end
 
 
