9edd3c945b80c04c51884612b55d5191387b1ea4
cellis222 <cellis_222@hotmail.com>
2012-11-30 17:31:20 +0000

    10-fold angry-validation

--- assignment4/CrossValidate.m
+++ assignment4/CrossValidate.m
@@ -1,63 +1,60 @@
 function [ classificationRate ] = CrossValidate( examples, labels )
-%CrossValidate uses 10-fold validation to estimate the error rate of the 6
-%trees created from training the examples
+%CrossValidate uses 10-fold validation to estimate the error rate of the
+%CBR created from training the examples
 
 classificationRate = 0;
 last = 0;
-
-targets = cell(6);
-for i=1:6
-    targets{i} = zeros(size(labels));
-    for j=1:size(labels)
-        targets{i}(j) = labels(j) == i;
-    end
-end
 
 confusionMatrix = zeros(6);
 
 for i=1:10
     % first will mark the index of the start of the fold, and last the end
     first = last + 1;
-    last = round(size(examples, 1)*i / 10);
-    
+    last = round(size(examples, 2)*i / 10);
+
     % split the examples from the fold
-    trainingSet = examples(~ismember(1:size(examples, 1), [first:last]), :);
-    testSet = examples(ismember(1:size(examples, 1), [first:last]), :);
-    testSetLabels = labels(ismember(1:size(labels, 1), [first:last]), :);
-    
-    % create the 6 trees for the fold, and classify the test set for each
-    trees = cell(1, 6);
-    for n=1:6
-        trainingSetTargets = targets{n}(~ismember(1:size(targets{n}, 1), [first:last]), :);
-        
-        % train a tree on the examples
-        trees{n} = DecisionTreeLearning(trainingSet, 1:1:45, trainingSetTargets);
-    end
+    trainingInputs = examples(~ismember(1:size(examples, 1), [first:last]), :);
+    trainingTargets = labels(~ismember(1:size(labels, 1), [first:last]), :);
+    validationInputs = examples(ismember(1:size(examples, 1), [first:last]), :);
+    validationTargets = labels(ismember(1:size(labels, 1), [first:last]), :);
 
-    x2.x = testSet;
-    x2.y = testSetLabels;
-    predictions = TestTrees(trees, x2);
-    
-    % compare to actual results and generate confusion matrix
+    cbr = CBRInit(trainingInputs, trainingTargets);
+
     correct = 0;
+    foldConfusionMatrix = zeros(6);
     
-    for m=1:size(testSetLabels)
-        predictedLabel = predictions.y(m);
-        actualLabel = testSetLabels(m);
-
+    for j=1:size(validationTargets, 1)
+        newcase = MakeCase(validationInputs(j,:), 0);
+        solvedcase = reuse(retrieve(cbr, newcase), newcase);
+        cbr = retain(cbr, solvedcase);
+        
+        %predictedLabel = predictions(j);
+        %nonZeroValues = find(validationTargets(:, j));
+        %actualLabel = nonZeroValues(1);
+        
+        predictedLabel = solvedcase.solution;% = [predictions solvedcase.solution];
+        actualLabel = validationTargets(j);
+        
         if predictedLabel == actualLabel
             correct = correct + 1;
         end
-        
+
         confusionMatrix(actualLabel, predictedLabel) = confusionMatrix(actualLabel, predictedLabel) + 1;
+        foldConfusionMatrix(actualLabel, predictedLabel) = foldConfusionMatrix(actualLabel, predictedLabel) + 1;
     end
-    
-    classificationRate = classificationRate + (correct / size(testSetLabels, 1));
+
+    % report fold F1 measure
+    fprintf('Fold %i\n:', i)
+    foldRecallAndPrecisionRates = getRecallAndPrecisionRates(foldConfusionMatrix);
+    foldF1Measures = mean(getF1Measures(foldRecallAndPrecisionRates))
+
+    classificationRate = classificationRate + (correct / size(validationTargets, 2));
 end
 
+fprintf('6-output:\n')
 confusionMatrix = confusionMatrix / 10
 
-recallAndPrecisionRates = GetRecallAndPrecisionRates(confusionMatrix)
-f1Measures = GetF1Measures(recallAndPrecisionRates)
+recallAndPrecisionRates = getRecallAndPrecisionRates(confusionMatrix)
+f1Measures = getF1Measures(recallAndPrecisionRates)
 
 classificationRate = classificationRate / 10
