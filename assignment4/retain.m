ad09e731ac9598f4f73ca6bea463ac047ebea3a4
Jack Bracewell <jbtwentythree@gmail.com>
2012-12-02 17:10:27 +0000

    experimental retain

--- assignment4/retain.m
+++ assignment4/retain.m
@@ -1,8 +1,26 @@
 function [ newcbr ] = retain( cbr, solvedcase )
 %retain updates cbr by storing solvedcase and returns a new CBR system.
+    solution = solvedcase.solution;
+    problem = solvedcase.problem;
+    weight = 0.5;
+    
+    average = cbr.categories{solution}.averageBinaryProblem;
+    
+    noOfCases = cbr.categories{solution}.noOfCases + 1;
+    cbr.categories{solution}.noOfCases = noOfCases;
+    average = average + ((problem / noOfCases) * weight);
+    
+    cbr.categories{solution}.averageBinaryProblem = average;
+        
+    newcbr = cbr;
+end
+
+
+function [ newcbr ] = OldRetain( cbr, solvedcase )
+%retain updates cbr by storing solvedcase and returns a new CBR system.
     existingcase = ExistsInCellArray(cbr.cases, solvedcase.problem);
     if existingcase == -1
-        cbr.cases{size(cbr, 1) + 1} = solvedcase;
+        cbr.categories{solution}.cases{size(cbr, 1) + 1} = solvedcase;
     else
         cbr.cases{existingcase}.typicality = cbr.cases{existingcase}.typicality + 1;
     end
